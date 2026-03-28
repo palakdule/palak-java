230495ece35bc0188defe28bb4364310f271bce6
palakdule <palakdule30@gmail.com>
2026-03-28 23:56:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Online Learning Platform";
+
+        if (str.contains("Offline")) {
+            System.out.println("The string contains 'Offline'.");
+        } else {
+            System.out.println("The string does not contain 'Offline'.");
+        }
+    }
+}

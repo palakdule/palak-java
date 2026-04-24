4d3e86c76ffc7f9a67bc8ca0d5d8603920070165
Palak Dule <palakdule30@gmail.com>
2026-04-24 18:43:02 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,20 @@
+public class MethodOverloading {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum of 2 ints: " + add(10, 20));
+        System.out.println("Sum of 3 ints: " + add(10, 20, 30));
+        System.out.println("Sum of 2 doubles: " + add(10.5, 20.5));
+    }
+}

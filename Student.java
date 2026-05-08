ad6bc6ca68d33afedc298c55653d6cc1146364e5
Palak Dule <palakdule30@gmail.com>
2026-05-08 20:58:51 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,13 +1,30 @@
 class Student {
+    private int rollNo;
+    private String name;
 
-    void display() {
-        System.out.println("Hello, this is a method.");
+    public void setRollNo(int r) {
+        rollNo = r;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public static void main(String[] args) {
-
         Student s1 = new Student();
 
-        s1.display();
+        s1.setRollNo(101);
+        s1.setName("Kunal");
+
+        System.out.println("Roll Number: " + s1.getRollNo());
+        System.out.println("Name: " + s1.getName());
     }
 }
